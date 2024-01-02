74d763f7d5b58cfc83be0dc465c47fa7cde5b250
whdnjsdyd111 <whdnjsdyd111@naver.com>
2024-01-02 11:56:42 +0900

    Arguments are modified

--- utils/arguments.py
+++ utils/arguments.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass, field
 from typing import Optional, Union, List
 
 from transformers import HfArgumentParser, TrainingArguments, Seq2SeqTrainingArguments
-from peft import PeftType, PromptTuningInit, PromptEncoderReparameterizationType
+from peft import PeftType, PromptTuningInit, LoftQConfig, PromptEncoderReparameterizationType
 
 from tasks.utils import *
 
@@ -180,36 +180,10 @@ class DynamicPeftArguments:
             "help": "Training with Peft or Fine-Tuning"
         }
     )
-    """PromptLearningConfig
-    This is the base configuration class to store the configuration of [`PrefixTuning`], [`PromptEncoder`], or
-    [`PromptTuning`].
-
-    Args:
-        num_virtual_tokens (`int`): The number of virtual tokens to use.
-        token_dim (`int`): The hidden embedding dimension of the base transformer model.
-        num_transformer_submodules (`int`): The number of transformer submodules in the base transformer model.
-        num_attention_heads (`int`): The number of attention heads in the base transformer model.
-        num_layers (`int`): The number of layers in the base transformer model.
-    """
+    # PromptLearningConfig
     num_virtual_tokens: int = field(default=None, metadata={"help": "Number of virtual tokens"})
-    token_dim: int = field(
-        default=None, metadata={"help": "The hidden embedding dimension of the base transformer model"}
-    )
-    num_transformer_submodules: Optional[int] = field(
-        default=None, metadata={"help": "Number of transformer submodules"}
-    )
-    num_attention_heads: Optional[int] = field(default=None, metadata={"help": "Number of attention heads"})
-    num_layers: Optional[int] = field(default=None, metadata={"help": "Number of transformer layers"})
-    """PromptTuningConfig
-    This is the configuration class to store the configuration of a [`PromptEmbedding`].
-
-    Args:
-        prompt_tuning_init (Union[[`PromptTuningInit`], `str`]): The initialization of the prompt embedding.
-        prompt_tuning_init_text (`str`, *optional*):
-            The text to initialize the prompt embedding. Only used if `prompt_tuning_init` is `TEXT`.
-        tokenizer_name_or_path (`str`, *optional*):
-            The name or path of the tokenizer. Only used if `prompt_tuning_init` is `TEXT`.
-    """
+    
+    # PromptTuningConfig
     prompt_tuning_init: Union[PromptTuningInit, str] = field(
         default=PromptTuningInit.RANDOM,
         metadata={"help": "How to initialize the prompt tuning parameters"},
@@ -220,44 +194,17 @@ class DynamicPeftArguments:
             "help": "The text to use for prompt tuning initialization. Only used if prompt_tuning_init is `TEXT`"
         },
     )
-    tokenizer_name_or_path: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": "The tokenizer to use for prompt tuning initialization. Only used if prompt_tuning_init is `TEXT`"
-        },
-    )
-    """PrefixTuningConfig
-    This is the configuration class to store the configuration of a [`PrefixEncoder`].
-
-    Args:
-        encoder_hidden_size (`int`): The hidden size of the prompt encoder.
-        prefix_projection (`bool`): Whether to project the prefix embeddings.
-    """
-    encoder_hidden_size: int = field(
-        default=None,
-        metadata={"help": "The hidden size of the encoder"},
-    )
+    
+    # PrefixTuningConfig
     prefix_projection: bool = field(
         default=False,
         metadata={"help": "Whether to project the prefix tokens"},
     )
-    """PromptEncoderConfig
-    This is the configuration class to store the configuration of a [`PromptEncoder`].
-
-    Args:
-        encoder_reparameterization_type (Union[[`PromptEncoderReparameterizationType`], `str`]):
-            The type of reparameterization to use.
-        encoder_hidden_size (`int`): The hidden size of the prompt encoder.
-        encoder_num_layers (`int`): The number of layers of the prompt encoder.
-        encoder_dropout (`float`): The dropout probability of the prompt encoder.
-    """
+    
+    # PromptEncoderConfig
     encoder_reparameterization_type: Union[str, PromptEncoderReparameterizationType] = field(
         default=PromptEncoderReparameterizationType.MLP,
         metadata={"help": "How to reparameterize the prompt encoder"},
-    )
-    encoder_hidden_size: int = field(
-        default=None,
-        metadata={"help": "The hidden size of the prompt encoder"},
     )
     encoder_num_layers: int = field(
         default=2,
@@ -267,38 +214,9 @@ class DynamicPeftArguments:
         default=0.0,
         metadata={"help": "The dropout of the prompt encoder"},
     )
-    """LoraConfig
-    This is the configuration class to store the configuration of a [`LoraModel`].
-
-    Args:
-        r (`int`): Lora attention dimension.
-        target_modules (`Union[List[str],str]`): The names of the modules to apply Lora to.
-        lora_alpha (`int`): The alpha parameter for Lora scaling.
-        lora_dropout (`float`): The dropout probability for Lora layers.
-        fan_in_fan_out (`bool`): Set this to True if the layer to replace stores weight like (fan_in, fan_out).
-            For example, gpt-2 uses `Conv1D` which stores weights like (fan_in, fan_out) and hence this should be set
-            to `True`.
-        bias (`str`): Bias type for Lora. Can be 'none', 'all' or 'lora_only'. If 'all' or 'lora_only', the
-            corresponding biases will be updated during training. Be aware that this means that, even when disabling
-            the adapters, the model will not produce the same output as the base model would have without adaptation.
-        modules_to_save (`List[str]`):List of modules apart from LoRA layers to be set as trainable
-            and saved in the final checkpoint.
-        layers_to_transform (`Union[List[int],int]`):
-            The layer indexes to transform, if this argument is specified, it will apply the LoRA transformations on
-            the layer indexes that are specified in this list. If a single integer is passed, it will apply the LoRA
-            transformations on the layer at this index.
-        layers_pattern (`str`):
-            The layer pattern name, used only if `layers_to_transform` is different from `None` and if the layer
-            pattern is not in the common layers pattern.
-    """
+    
+    # LoraConfig
     r: int = field(default=8, metadata={"help": "Lora attention dimension"})
-    target_modules: Optional[Union[List[str], str]] = field(
-        default=None,
-        metadata={
-            "help": "List of module names or regex expression of the module names to replace with Lora."
-            "For example, ['q', 'v'] or '.*decoder.*(SelfAttention|EncDecAttention).*(q|v)$' "
-        },
-    )
     lora_alpha: int = field(default=8, metadata={"help": "Lora alpha"})
     lora_dropout: float = field(default=0.0, metadata={"help": "Lora dropout"})
     fan_in_fan_out: bool = field(
@@ -306,105 +224,16 @@ class DynamicPeftArguments:
         metadata={"help": "Set this to True if the layer to replace stores weight like (fan_in, fan_out)"},
     )
     bias: str = field(default="none", metadata={"help": "Bias type for Lora. Can be 'none', 'all' or 'lora_only'"})
-    modules_to_save: Optional[List[str]] = field(
-        default=None,
-        metadata={
-            "help": "List of modules apart from LoRA layers to be set as trainable and saved in the final checkpoint. "
-            "For example, in Sequence Classification or Token Classification tasks, "
-            "the final layer `classifier/score` are randomly initialized and as such need to be trainable and saved."
-        },
-    )
     init_lora_weights: bool = field(
         default=True,
-        metadata={
-            "help": (
-                "Whether to initialize the weights of the Lora layers with their default initialization. Don't change "
-                "this setting, except if you know exactly what you're doing."
-            ),
-        },
+        metadata={"help": "Whether to initialize the weights of the Lora layers."},
     )
-    # layers_to_transform: Optional[Union[List[int], int]] = field(
-    #     default=None,
-    #     metadata={
-    #         "help": "The layer indexes to transform, is this argument is specified, PEFT will transform only the layers indexes that are specified inside this list. If a single integer is passed, PEFT will transform only the layer at this index."
-    #     },
-    # )
-    layers_pattern: Optional[str] = field(
-        default=None,
-        metadata={
-            "help": "The layer pattern name, used only if `layers_to_transform` is different to None and if the layer pattern is not in the common layers pattern."
-        },
-    )
-    """IA3Config
-    This is the configuration class to store the configuration of a [`IA3Model`].
-
-    Args:
-        target_modules (`Union[List[str],str]`):
-            The names of the modules to apply (IA)^3 to.
-        feedforward_modules (`Union[List[str],str]`):
-            The names of the modules to be treated as feedforward modules, as in the original paper.
-        fan_in_fan_out (`bool`):
-            Set this to True if the layer to replace stores weight like (fan_in, fan_out). For example, gpt-2 uses
-            `Conv1D` which stores weights like (fan_in, fan_out) and hence this should be set to `True`.
-        modules_to_save (`List[str]`):
-            List of modules apart from (IA)^3 layers to be set as trainable and saved in the final checkpoint.
-        init_ia3_weights (`bool`):
-            Whether to initialize the vectors in the (IA)^3 layers, defaults to `True`.
-    """
-    target_modules: Optional[Union[List[str], str]] = field(
-        default=None,
-        metadata={
-            "help": "List of module names or regex expression of the module names to replace with ia3."
-            "For example, ['q', 'v'] or '.*decoder.*(SelfAttention|EncDecAttention).*(q|v)$' "
-        },
-    )
-    feedforward_modules: Optional[Union[List[str], str]] = field(
-        default=None,
-        metadata={
-            "help": "List of module names or a regex expression of module names which are feedforward"
-            "For example, ['output.dense']"
-        },
-    )
-    fan_in_fan_out: bool = field(
-        default=False,
-        metadata={"help": "Set this to True if the layer to replace stores weight like (fan_in, fan_out)"},
-    )
-    modules_to_save: Optional[List[str]] = field(
-        default=None,
-        metadata={
-            "help": "List of modules apart from (IA)^3 layers to be set as trainable and saved in the final checkpoint. "
-            "For example, in Sequence Classification or Token Classification tasks, "
-            "the final layer `classifier/score` are randomly initialized and as such need to be trainable and saved."
-        },
-    )
-    init_ia3_weights: bool = field(
-        default=True,
-        metadata={"help": "Whether to initialize the vectors in the (IA)^3 layers."},
-    )
-    """AdaptionPromptConfig
-    Stores the configuration of an [`AdaptionPromptModel`].
-    """
-
-    target_modules: str = field(
-        default=None, metadata={"help": "Name of the attention submodules to insert adaption prompts into."}
-    )
+    
+    # AdaptionPromptConfig
     adapter_len: int = field(default=None, metadata={"help": "Number of adapter tokens to insert"})
     adapter_layers: int = field(default=None, metadata={"help": "Number of adapter layers (from the top)"})
-    """AdaLoraConfig
-    This is the configuration class to store the configuration of a [`~peft.AdaLora`].
-
-    Args:
-        target_r (`int`): The target average rank of incremental matrix.
-        init_r (`int`): The initial rank for each incremental matrix.
-        tinit (`int`): The steps of initial fine-tuning warmup.
-        tfinal (`int`): The step of final fine-tuning.
-        deltaT (`int`): The time internval between two budget allocations.
-        beta1 (`float`): The hyperparameter of EMA for sensitivity smoothing.
-        beta2 (`float`): The hyperparameter of EMA for undertainty quantification.
-        orth_reg_weight (`float`): The coefficient of orthogonal regularization.
-        total_step (`int`): The total training steps that should be specified before training.
-        rank_pattern (`list`): The allocated rank for each weight matrix by RankAllocator.
-    """
+    
+    # AdaLoraConfig
     target_r: int = field(default=8, metadata={"help": "Target Lora matrix dimension."})
     init_r: int = field(default=12, metadata={"help": "Intial Lora matrix dimension."})
     tinit: int = field(default=0, metadata={"help": "The steps of initial warmup."})
@@ -414,7 +243,6 @@ class DynamicPeftArguments:
     beta2: float = field(default=0.85, metadata={"help": "Hyperparameter of EMA."})
     orth_reg_weight: float = field(default=0.5, metadata={"help": "The orthogonal regularization coefficient."})
     total_step: Optional[int] = field(default=None, metadata={"help": "The total training steps."})
-    rank_pattern: Optional[dict] = field(default=None, metadata={"help": "The saved rank pattern."})
 
 
 def get_args():
