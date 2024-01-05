125dc78a58005415b2d471d6e52d9bf560b2f0ad
whdnjsdyd111 <whdnjsdyd111@naver.com>
2024-01-05 14:39:13 +0900

    full-fine-tuning k-shot-10 learning

---
+++ run_script/peft/prefix-tuning/bert-base-uncased/run_glue_small-10-shot.sh
@@ -0,0 +1,38 @@
+export MODEL_NAME=bert-base-uncased
+export TASK_NAME=glue
+export CUDA_VISIBLE_DEVICES=0
+export PEFT_TYPE=PREFIX_TUNING
+
+max_seq_length=128
+bs=32
+epoch=20
+weight_decay=1e-5
+virtual_tokens_list="20"
+
+for DATASET_NAME in cola mrpc rte stsb wnli; do
+  for lr in 1e-2 1e-3; do
+    for var in $virtual_tokens_list; do
+      python run.py \
+        --model_name_or_path $MODEL_NAME \
+        --run_name $TASK_NAME-$DATASET_NAME-$MODEL_NAME-$lr-$PEFT_TYPE-$var \
+        --task_name $TASK_NAME \
+        --dataset_name $DATASET_NAME \
+        --do_train \
+        --do_eval \
+        --max_seq_length $max_seq_length \
+        --per_device_train_batch_size $bs \
+        --per_device_eval_batch_size $bs \
+        --learning_rate $lr \
+        --num_train_epochs $epoch \
+        --weight_decay $weight_decay \
+        --output_dir checkpoints/PEFT/PREFIX_TUNING/$TASK_NAME-$DATASET_NAME-$MODEL_NAME-$lr-$PEFT_TYPE-$var// \
+        --overwrite_output_dir \
+        --seed 1 \
+        --save_strategy no \
+        --evaluation_strategy epoch \
+        --peft_type $PEFT_TYPE \
+        --k_shot_example 0 \
+        --num_virtual_tokens $var
+    done;
+  done;
+done
