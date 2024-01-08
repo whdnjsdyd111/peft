76b26eebd9f4981b619589faa2cc8a9a3af552c8
whdnjsdyd111 <whdnjsdyd111@naver.com>
2024-01-08 16:36:30 +0900

    Modify full-fine-tuning base setting

--- run_script/full-fine-tuning/bert-base-uncased/run_glue_large.sh
+++ run_script/full-fine-tuning/bert-base-uncased/run_glue_large.sh
@@ -8,7 +8,7 @@ epoch=10
 weight_decay=1e-5
 
 for DATASET_NAME in mnli qnli qqp sst2; do
-  for lr in 5e-4 5e-5; do
+  for lr in 1e-4 5e-4 1e-3; do
     python run.py \
       --model_name_or_path $MODEL_NAME \
       --run_name $TASK_NAME-$DATASET_NAME-$MODEL_NAME-$lr \
@@ -16,6 +16,7 @@ for DATASET_NAME in mnli qnli qqp sst2; do
       --dataset_name $DATASET_NAME \
       --do_train \
       --do_eval \
+      --do_predict \
       --max_seq_length $max_seq_length \
       --per_device_train_batch_size $bs \
       --per_device_eval_batch_size $bs \
@@ -26,6 +27,9 @@ for DATASET_NAME in mnli qnli qqp sst2; do
       --overwrite_output_dir \
       --seed 1 \
       --save_strategy no \
-      --evaluation_strategy epoch
+      --evaluation_strategy epoch \
+      --save_strategy epoch \
+      --load_best_model_at_end \
+      --save_total_limit 1 
   done;
 done
--- run_script/full-fine-tuning/bert-base-uncased/run_glue_small-10-shot.sh
+++ run_script/full-fine-tuning/bert-base-uncased/run_glue_small-10-shot.sh
@@ -8,7 +8,7 @@ epoch=20
 weight_decay=1e-5
 
 for DATASET_NAME in cola mrpc rte stsb wnli; do
-  for lr in 5e-4 5e-5; do
+  for lr in 1e-4 5e-4 1e-3; do
     python run.py \
       --model_name_or_path $MODEL_NAME \
       --run_name $TASK_NAME-$DATASET_NAME-$MODEL_NAME-$lr-k-shot-10 \
@@ -16,6 +16,7 @@ for DATASET_NAME in cola mrpc rte stsb wnli; do
       --dataset_name $DATASET_NAME \
       --do_train \
       --do_eval \
+      --do_predict \
       --max_seq_length $max_seq_length \
       --per_device_train_batch_size $bs \
       --per_device_eval_batch_size $bs \
@@ -27,6 +28,9 @@ for DATASET_NAME in cola mrpc rte stsb wnli; do
       --seed 1 \
       --save_strategy no \
       --evaluation_strategy epoch \
+      --save_strategy epoch \
+      --load_best_model_at_end \
+      --save_total_limit 1 \
       --k_shot_example 10
   done;
 done
