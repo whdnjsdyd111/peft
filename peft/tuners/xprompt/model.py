c02643a2871702ad18138b7c84a192b997dfb807
whdnjsdyd111 <whdnjsdyd111@naver.com>
2024-02-10 15:58:13 +0900

    typo

--- peft/tuners/xprompt/model.py
+++ peft/tuners/xprompt/model.py
@@ -71,16 +71,22 @@ class XPromptEmbedding(torch.nn.Module):
         total_virtual_tokens = config.num_virtual_tokens * config.num_transformer_submodules
         self.embedding = torch.nn.Embedding(total_virtual_tokens, config.token_dim)
         self.token_mask = torch.ones(total_virtual_tokens)
-        # self.piece_mask = torch.nn.ones()
+        self.piece_mask = torch.ones(total_virtual_tokens, config.token_dim)
+        if config.token_dim % config.token_pieces > 0:
+            raise ValueError("The number of token_pieces for the token dimension does not perfectly divide.")
         
         self.to_prune = {
             self.token_prefix: set(), 
-            # "piece-level": set(),
-            }
+            self.piece_prefix: {}
+        }
         self.kept_prune = {
             self.token_prefix: set(range(total_virtual_tokens)), 
-            # "piece-level": set(),
+            self.piece_prefix: {
+                f"{self.token_prefix}:{token}": set(range(config.token_pieces)) 
+                for token in total_virtual_tokens
+            }
         }
+        
         
         if config.xprompt_tuning_init == XPromptTuningInit.TEXT:
             from transformers import AutoTokenizer
@@ -108,7 +114,7 @@ class XPromptEmbedding(torch.nn.Module):
     
     def forward(self, indices):
         # Just get embeddings
-        prompt_embeddings = self.embedding(indices)
+        prompt_embeddings = self.embedding(indices) * self.piece_mask
         return prompt_embeddings
 
     def batch_token_importance(self):
@@ -123,6 +129,24 @@ class XPromptEmbedding(torch.nn.Module):
         importance = torch.einsum(
             "ew,ew->e",
             [embed, d_embed],
+        )
+        
+        importance *= mask
+        importance = importance.abs().detach()
+        
+        return importance, mask
+    
+    def batch_piece_importance(self):
+        split_size = self.config.token_dim // self.config.token_pieces
+        split_embed = torch.split(self.embedding.weight, split_size, dim=1)
+        split_d_embed = torch.split(self.embedding.weight.grad, split_size, dim=1)
+        mask = self.piece_mask[:, torch.arange(self.config.token_dim) % split_size == 0].to(
+            self.embedding.weight.device
+        )
+        
+        importance = torch.stack(
+            [torch.einsum("li,li->l", piece, grad) 
+             for piece, grad in zip(split_embed, split_d_embed)], dim=1
         )
         
         importance *= mask
@@ -146,12 +170,8 @@ class XPromptEmbedding(torch.nn.Module):
         model.eval()
         trainer.optimizer.zero_grad()
         
-        embed_importance = {
-            self.token_prefix: torch.zeros(self.total_virtual_tokens).to(device)
-            # "piece-level": torch.zeros(total_virtual_tokens).to(device)
-        }
-        denoms = {attn_type: val.clone() for attn_type, val in embed_importance.items()}
-        profile = {}
+        embed_importance = torch.zeros(self.total_virtual_tokens).to(device)
+        denoms = embed_importance.clone()
         
         for idx, data in enumerate(trainer.get_train_dataloader()):
             input_ids = data["input_ids"]
@@ -169,41 +189,123 @@ class XPromptEmbedding(torch.nn.Module):
             
             importance, denom = self.batch_token_importance() 
             
-            embed_importance[self.token_prefix] += importance
-            denoms[self.token_prefix] += denom
+            embed_importance += importance
+            denoms += denom
         
         if is_training:
             model.train()
         
         # token Normalize
-        embed_importance[self.token_prefix] /= denoms[self.token_prefix]
-        profile[self.token_prefix] = self.token_profile(embed_importance)
+        embed_importance /= denoms
+        profile = self.token_profile(embed_importance)
         
         # prune the tokens
         for desc, _ in profile[self.token_prefix]:
             idx = int(desc.split(":")[1])
             self.to_prune[self.token_prefix].add(idx)
             self.kept_prune[self.token_prefix].remove(idx)
+            self.to_prune[self.piece_prefix][desc] = self.kept_prune[self.piece_prefix].pop(desc)
             self.token_mask[idx] = 0
+            self.piece_mask[idx] = 0
         
         logger.info(f"*** {colorstr('cyan', 'bold', 'token importance scores')} to be removed ***\n"
                     f"{list(f'{token[1].item():.5f}' for token in profile[self.token_prefix])}\n"
                     f"{list(token for token, _ in profile[self.token_prefix])}\n")
-        logger.info(f"*** {colorstr('cyan', 'bold', 'pruned')} ***\n{self.to_prune}")
-        logger.info(f"*** {colorstr('cyan', 'bold', 'kept')} ***\n{self.kept_prune}\n")
-        logger.info(self.token_mask)
+        logger.info(f"*** {colorstr('cyan', 'bold', 'pruned')} ***\n{self.to_prune[self.token_prefix]}")
+        logger.info(f"*** {colorstr('cyan', 'bold', 'kept')} ***\n{self.kept_prune[self.token_prefix]}\n")
+        logger.info(f"*** {colorstr('cyan', 'bold', 'token mask')} ***\n"self.token_mask)
+    
+    def estimate_piece_importance(self, trainer, current_step):
+        """Train the model for one epoch to prune the negative token"""
+        
+        # only prune the token in target step.
+        if current_step != self.config.prune_step:
+            return
+        
+        logger.info(f"*** {colorstr('cyan', 'bold', 'Pruning Pieces')} ***")
+        
+        model = trainer.model
+        is_training = model.training
+        device = self.embedding.weight.device
+        
+        model.eval()
+        trainer.optimizer.zero_grad()
+        
+        embed_importance = torch.zeros(self.total_virtual_tokens, self.config.token_pieces).to(device)
+        denoms = embed_importance.clone()
+        
+        for idx, data in enumerate(trainer.get_train_dataloader()):
+            input_ids = data["input_ids"]
+            attention_mask = data["attention_mask"]
+            labels = data["labels"]
+            
+            output = model.forward(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                labels=labels,
+            )
+            
+            self.embedding.weight.retain_grad()
+            output.loss.backward()
+            
+            importance, denom = self.batch_piece_importance() 
+            
+            embed_importance += importance
+            denoms += denom
+        
+        if is_training:
+            model.train()
+        
+        # piece Normalize
+        embed_importance /= denoms
+        profile = self.piece_profile(embed_importance)
+        
+        # prune the pieces
+        split_size = self.config.token_dim // self.config.token_pieces
+        for token, _ in profile.items():
+            if self.to_prune[self.piece_prefix].get(token) is None: self.to_prune[self.piece_prefix][token] = set()
+            for desc, _ in profile[token]:
+                idx = int(desc.split(":")[1])
+                self.to_prune[self.piece_prefix][token].add(idx)
+                self.kept_prune[self.piece_prefix][token].remove(idx)
+                self.piece_mask[int(token.split(":")[1])][idx*split_size:(idx+1)*split_size] = 0
+        
+        logger.info(f"*** {colorstr('cyan', 'bold', 'piece importance scores')} to be removed ***\n"
+                    f"{list(f'{token[1].item():.5f}' for token in profile[self.token_prefix])}\n"
+                    f"{list(token for token, _ in profile[self.token_prefix])}\n")
+        logger.info(f"*** {colorstr('cyan', 'bold', 'pruned')} ***\n{self.to_prune[self.token_prefix]}")
+        logger.info(f"*** {colorstr('cyan', 'bold', 'kept')} ***\n{self.kept_prune[self.token_prefix]}\n")
+        logger.info(f"*** {colorstr('cyan', 'bold', 'token mask')} ***\n"self.token_mask)
     
     def token_profile(self, importance):
         target_token_length = int(self.total_virtual_tokens * self.config.token_ratio)
         
         # create profile
         profile = {
-            f"{self.token_prefix}:{token}": importance[self.token_prefix][token]
-            for token in range(len(importance[self.token_prefix]))
+            f"{self.token_prefix}:{token}": importance[token]
+            for token in range(len(importance))
         }
         # select kept tokens
         profile = {k: v for k, v in profile.items() if int(k.split(":")[1]) in self.kept_prune[self.token_prefix]}
         # sort tokens
         profile = sorted(profile.items(), key=lambda x: x[1])[:target_token_length]
+        
+        return profile
+
+    def piece_profile(self, importance):
+        target_piece_length = int(self.config.token_pieces * self.config.piece_ratio)
+        
+        # create profile
+        profile = {
+            f"{self.token_prefix}:{token}": {
+                f"{self.piece_prefix}:{piece}": importance[token][piece]
+                for piece in range(len(importance[token]))
+            }
+            for token in range(len(importance))
+        }
+        # select kept tokens
+        profile = {k: v for k, v in profile.items() if int (k.split(":")[1]) in self.kept_prune[self.token_prefix]}
+        for k, v in profile.items():
+            profile[k] = sorted(profile[k].items(), key=lambda x: x[1])[:target_piece_length]
         
         return profile
