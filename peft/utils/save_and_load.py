930b878c03c379aff42bf369f9b2f7c389d813ee
whdnjsdyd111 <whdnjsdyd111@naver.com>
2024-02-11 22:26:12 +0900

    annotation not tensor value

--- peft/utils/save_and_load.py
+++ peft/utils/save_and_load.py
@@ -104,8 +104,8 @@ def get_peft_model_state_dict(
         to_return["prompt_embeddings"] = prompt_embeddings
         
         if config.peft_type == PeftType.XPROMPT_TUNING:
-            to_return["to_prune"] = model.prompt_encoder[adapter_name].to_prune
-            to_return["kept_prune"] = model.prompt_encoder[adapter_name].kept_prune
+            # to_return["to_prune"] = model.prompt_encoder[adapter_name].to_prune
+            # to_return["kept_prune"] = model.prompt_encoder[adapter_name].kept_prune
             to_return["token_mask"] = model.prompt_encoder[adapter_name].token_mask
             to_return["piece_mask"] = model.prompt_encoder[adapter_name].piece_mask
     else:
@@ -201,8 +201,8 @@ def set_peft_model_state_dict(model, peft_model_state_dict, adapter_name="defaul
         )
         
         if config.peft_type == PeftType.XPROMPT_TUNING:
-            model.prompt_encoder[adapter_name].to_prune = peft_model_state_dict["to_prune"]
-            model.prompt_encoder[adapter_name].kept_prune = peft_model_state_dict["kept_prune"]
+            # model.prompt_encoder[adapter_name].to_prune = peft_model_state_dict["to_prune"]
+            # model.prompt_encoder[adapter_name].kept_prune = peft_model_state_dict["kept_prune"]
             model.prompt_encoder[adapter_name].token_mask = peft_model_state_dict["token_mask"]
             model.prompt_encoder[adapter_name].piece_mask = peft_model_state_dict["piece_mask"]
     
