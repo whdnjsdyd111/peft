afad0c983032d05155456e083fc6272730b8b180
whdnjsdyd111 <whdnjsdyd111@naver.com>
2024-02-18 16:43:09 +0900

    generation_num_beams

--- utils/arguments.py
+++ utils/arguments.py
@@ -196,6 +196,10 @@ class DynamicTrainingArguments(Seq2SeqTrainingArguments):
             "than this will be truncated, sequences shorter will be padded."
         },
     )
+    generation_num_beams: Optional[int] = field(
+        default=1, 
+        metadata={"help": "Number of beams to use for evaluation."}
+    )
 
 @dataclass
 class DynamicPeftArguments:
